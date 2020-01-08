1c1ed1b52d121effff011f180f5a776575c98989
IGL-T <iristeeuwen95@gmail.com>
2020-01-08 20:33:41 +0100

    first commit
    Signed-off-by: IGL-T <iristeeuwen95@gmail.com>

---
+++ readme.md
@@ -0,0 +1,2 @@
+https://github.com/kittykatattack/learningPixi
+--save voor dependency toevoegen
---
+++ src/example.js
@@ -0,0 +1,55 @@
+let type = "WebGL"
+if(!PIXI.utils.isWebGLSupported()){
+    type = "canvas"
+}
+
+PIXI.utils.sayHello(type)
+
+//Create a Pixi Application
+let app = new PIXI.Application({
+        width: 1280,         // default: 800
+        height: 720,        // default: 600
+        antialias: true,    // default: false
+        transparent: false, // default: false
+        resolution: 1       // default: 1
+    }
+);
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+PIXI.loader
+    .add("assets/images/pixcat.png")
+    .add("assets/images/nebula.png")
+    .load(()=>{
+
+        //background
+        let background = new PIXI.extras.TilingSprite(
+            PIXI.loader.resources["assets/images/nebula.png"].texture,1024,1024
+        );
+        background.position.x=640;
+        background.position.y=360;
+        background.tilePosition.x=512;
+        background.tilePosition.y=512;
+        background.width=1280*2;
+        background.height=720*2;
+        background.anchor.set(0.5);
+        app.stage.addChild(background);
+
+        // cat
+        let sprite = new PIXI.Sprite(
+            PIXI.loader.resources["assets/images/pixcat.png"].texture
+        );
+        sprite.width=128;
+        sprite.height=128;
+        app.stage.addChild(sprite);
+
+        //update
+        app.ticker.add((delta)=>{
+            //background.tilePosition.x-=0.1;
+            background.rotation+=0.005
+
+        });
+});
+
+//
